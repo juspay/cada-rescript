664713e9f124178aac88d424b6ed0c6a46c49b5a
Jignyas Anand Siripurapu <jignyas.s@juspay.in>
2025-06-02 13:48:51 +0530

    consolidate all changes as single file + error handling +moduleName fix

--- python/main.py
+++ python/main.py
@@ -20,13 +20,13 @@ def clone_repo(repo_url, local_path):
     else:
         print(f'Repository already exists at {local_path}')
 
-def get_changed_files(branch_name, new_commit, local_path):
+def get_changed_files(branch_or_commit, new_commit, local_path):
     old_cwd = os.getcwd()
     os.chdir(local_path)
 
     try:
-        subprocess.run(['git', 'checkout', '-f', branch_name], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        commit = subprocess.run(['git', 'rev-parse', branch_name], check=True, stdout=subprocess.PIPE).stdout.decode().strip()
+        subprocess.run(['git', 'checkout', '-f', branch_or_commit], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        commit = subprocess.run(['git', 'rev-parse', branch_or_commit], check=True, stdout=subprocess.PIPE).stdout.decode().strip()
         diff = subprocess.run(['git', 'diff', '--name-only', commit, new_commit], check=True, stdout=subprocess.PIPE).stdout.decode()
 
         files = [file for file in diff.split('\n') if file.strip() and file.endswith('.res')]
@@ -39,60 +39,65 @@ def main():
     args = sys.argv[1:]
 
     if len(args) != 5:
-        print('Usage: python rescript_differ.py <repo_url> <local_path> <branch_name> <current_commit> <output_dir>')
+        print('Usage: python rescript_differ.py <repo_url> <local_path> <branch_or_commit> <current_commit> <output_dir>')
         sys.exit(1)
 
-    repo_url, local_repo_path, branch_name, current_commit, output_dir = args
+    repo_url, local_repo_path, branch_or_commit, current_commit, output_dir = args
 
-    # try:
-    RS_LANGUAGE = Language(tree_sitter_rescript.language())
-    parser = Parser(RS_LANGUAGE)
-    clone_repo(repo_url, local_repo_path)
+    try:
+        RS_LANGUAGE = Language(tree_sitter_rescript.language())
+        parser = Parser(RS_LANGUAGE)
+        clone_repo(repo_url, local_repo_path)
 
-    print('Getting changed files...')
-    changed_files = get_changed_files(branch_name, current_commit, local_repo_path)
-    print(f'Found {len(changed_files)} changed ReScript files')
+        print('Getting changed files...')
+        changed_files = get_changed_files(branch_or_commit, current_commit, local_repo_path)
+        print(f'Found {len(changed_files)} changed ReScript files')
 
-    file_node_dict = defaultdict(list)
+        file_node_dict = defaultdict(list)
 
-    file_paths = [os.path.join(local_repo_path, file) for file in changed_files]
+        file_paths = [os.path.join(local_repo_path, file) for file in changed_files]
 
-    print('Processing modules for previous commit...')
-    for changed_file in file_paths:
-        if os.path.exists(changed_file):
-            module_name = extract_module_name(changed_file)
-            ast = parser.parse(open(changed_file, "rb").read())
-            file_node_dict[module_name].append(ast)
+        print('Processing modules for previous commit...')
+        for changed_file in file_paths:
+            if os.path.exists(changed_file):
+                module_name = extract_module_name(changed_file)
+                ast = parser.parse(open(changed_file, "rb").read())
+                file_node_dict[module_name].append(ast)
 
-    print('Switching to current commit...')
-    old_cwd = os.getcwd()
-    os.chdir(local_repo_path)
-    subprocess.run(['git', 'checkout', '-f', current_commit], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    os.chdir(old_cwd)
+        print('Switching to current commit...')
+        old_cwd = os.getcwd()
+        os.chdir(local_repo_path)
+        subprocess.run(['git', 'checkout', '-f', current_commit], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        os.chdir(old_cwd)
 
-    print('Processing modules for current commit...')
-    for changed_file in file_paths:
-        if os.path.exists(changed_file):
-            module_name = extract_module_name(changed_file)
-            ast = parser.parse(open(changed_file, "rb").read())
-            file_node_dict[module_name].append(ast)
+        print('Processing modules for current commit...')
+        for changed_file in file_paths:
+            if os.path.exists(changed_file):
+                module_name = extract_module_name(changed_file)
+                ast = parser.parse(open(changed_file, "rb").read())
+                file_node_dict[module_name].append(ast)
 
-    print('Generating changes...')
+        print('Generating changes...')
 
-    os.makedirs(output_dir, exist_ok=True)
+        os.makedirs(output_dir, exist_ok=True)
+        all_changes = []
+        for module_name, asts_list in file_node_dict.items():
+            if len(asts_list) == 2:
+                old_node, new_node = asts_list
+                diff = RescriptFileDiff(old_node, new_node, module_name)
+                changes = diff.process_files()
+                out_path = os.path.join(output_dir, f"{module_name}_changes.json")
+                with open(out_path, "w") as f:
+                    json.dump(changes.to_dict(), f, indent=2)
 
-    for module_name, asts_list in file_node_dict.items():
-        if len(asts_list) == 2:
-            old_node, new_node = asts_list
-            diff = RescriptFileDiff(old_node, new_node)
-            changes = diff.process_files()
-            out_path = os.path.join(output_dir, f"{module_name}_changes.json")
-            with open(out_path, "w") as f:
-                json.dump(changes.to_dict(), f, indent=2)
+                all_changes.append(changes.to_dict())
 
-            print(f"Detailed changes written to {out_path}")
-
-
+                print(f"Detailed changes written to {out_path}")
+        with open(os.path.join(output_dir, "detailed_changes.json"), "w") as f:
+            json.dump(all_changes, f, indent=3)
+    except Exception as e:
+        print("ERROR - ", e)
+        pass
 
 if __name__ == '__main__':
     main()
